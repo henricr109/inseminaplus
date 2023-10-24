bc1fb5d96ab6dea0e60155023a81b065d696d453
Henri costa rios <henricostarios@gmail.com>
2023-10-23 22:18:51 -0300

    Update codigo do kuan V1
    modificação do CrudProduct original do kuan V1

---
+++ src/main/java/com/inseminaplus/spring/security/mongodb/controllers/ProductController.java
@@ -0,0 +1,108 @@
+package com.inseminaplus.spring.security.mongodb.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import  com.inseminaplus.spring.security.mongodb.models.Product;
+import com.inseminaplus.spring.security.mongodb.repository.ProductRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/Product")
+    public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required = false) String name) {
+        try {
+            List<Product> products = new ArrayList<Product>();
+
+            if (name == null)
+                productRepository.findAll().forEach(products::add);
+            else
+                productRepository.findByTitleContaining(name).forEach(products::add);
+
+            if (products.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getproductById(@PathVariable("id") String id) {
+        Optional<Product> productData = productRepository.findById(id);
+
+        if (productData.isPresent()) {
+            return new ResponseEntity<>(productData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        try {
+            Product _product = productRepository.save(new Product(product.getName(), product.getCategory(), product.getStock(), product.getValue()));
+            return new ResponseEntity<>(_product, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+        Optional<Product> productData = productRepository.findById(id);
+
+        if (productData.isPresent()) {
+            Product _product = productData.get();
+            _product.setName(product.getName());
+            _product.setCategory(product.getCategory());
+            _product.setStock(product.getStock());
+            return new ResponseEntity<>(productRepository.save(_product), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") String id) {
+        try {
+            productRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/products")
+    public ResponseEntity<HttpStatus> deleteAllProducts() {
+        try {
+            productRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/main/java/com/inseminaplus/spring/security/mongodb/models/Product.java
@@ -0,0 +1,77 @@
+package com.inseminaplus.spring.security.mongodb.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private String category;
+    private String stock;
+    private String value;
+
+    public Product() {
+
+    }
+
+    public Product(String name, String category, String stock, String value) {
+        this.name = name;
+        this.category = category;
+        this.stock = stock;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", stock='" + stock + '\'' +
+                ", value='" + value + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
