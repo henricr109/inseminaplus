1a647f765feb79ad364cacd4767fbc56e5b9e980
Henri costa rios <henricostarios@gmail.com>
2023-10-27 16:07:29 -0300

    update
    update

--- src/main/java/com/inseminaplus/spring/security/mongodb/payload/request/LoginRequest.java
+++ src/main/java/com/inseminaplus/spring/security/mongodb/payload/request/LoginRequest.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 public class LoginRequest {
   @NotBlank
   private String email;
-
   @NotBlank
   private String password;
 
--- src/main/java/com/inseminaplus/spring/security/mongodb/repository/UserRepository.java
+++ src/main/java/com/inseminaplus/spring/security/mongodb/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
   Optional<User> findByUsername(String username);
+  Optional<User> findByEmail(String email);
 
   Boolean existsByUsername(String username);
 
