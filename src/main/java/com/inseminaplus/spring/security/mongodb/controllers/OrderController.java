e9b3827b878ba4349dbfe2c0070facd60f3ad8e3
Henri costa rios <119058648+henricr109@users.noreply.github.com>
2023-11-06 16:35:39 -0300

    update exceptions
    exclusão de service de exception pois não é necessario mais

--- src/main/java/com/inseminaplus/spring/security/mongodb/controllers/OrderController.java
+++ src/main/java/com/inseminaplus/spring/security/mongodb/controllers/OrderController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.inseminaplus.spring.security.mongodb.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
